d1fffa0f6c14012e39f52eef83168b404440e384
tangethan79 <50676213+tangethan79@users.noreply.github.com>
2021-05-11 19:47:35 -0400

    master arduino code

---
+++ Prototype Unification/Motor Slave/motor_slave.ino
@@ -0,0 +1,26 @@
+// for addressing, 1 is master, 2 is motor slave, and 3 is OLED slave
+#include <Wire.h>
+
+void setup()
+{
+    Wire.begin(2);                // join i2c bus with address #2
+    Wire.onReceive(receiveEvent); // register event
+    Serial.begin(9600);     
+}
+
+void loop()
+{
+
+}
+
+void onReceive()
+{  
+    char message[2];
+    int i = 0;
+    while(1 < Wire.available()) // loop through all but the last
+    {
+        message[i] = Wire.read(); // receive byte as a character
+    }
+
+    // custom variable update here
+}
