b7eb81135a37c551dd4ae133953ab32df55d7b8c
divyaK15 <Divya.p.k15@gmail.com>
2021-07-20 19:07:19 -0400

    updated code

--- LEDs/LEDs.ino
+++ LEDs/LEDs.ino
@@ -69,35 +69,32 @@ void loop() {
   }
 
  
-//  //turn all the lights blue and flash 
-//  for(int i=0; i<NUM_LEDS; i++){
-//    leds[i] = CRGB(255, 0, 255);
-//    FastLED.setBrightness(6*i);
-//    FastLED.show();
-//    delay(50);
-//  }
-//
-//   for(int i=NUM_LEDS; i>0; i--){
-//    leds[i] = CRGB(0, 213, 255);
-//    FastLED.setBrightness(60-2*i);
-//    FastLED.show();
-//    delay(50);
-//  }
-
-//  //change the light colours
-//  for(int i=0; i<NUM_LEDS; i++){
-//    leds[i] = CRGB(255, 90, 0);
-//    FastLED.setBrightness(6*i);
-//    FastLED.show();
-//    delay(100);
-//  }
-//
-//   for(int i=NUM_LEDS; i>0; i--){
-//    leds[i] = CRGB(255, 40, 0);
-//    FastLED.setBrightness(60-2*i);
-//    FastLED.show();
-//    delay(100);
-//  } 
+  //turn all the lights blue and flash 
+  for(int i=0; i<NUM_LEDS; i++){
+    leds[i] = CRGB(255, 0, 255);
+    FastLED.setBrightness(6*i);
+    FastLED.show();
+    delay(50);
+    }
+   for(int i=NUM_LEDS; i>0; i--){
+    leds[i] = CRGB(0, 213, 255);
+    FastLED.setBrightness(60-2*i);
+    FastLED.show();
+    delay(50);
+  }
+  //change the light colours
+  for(int i=0; i<NUM_LEDS; i++){
+   leds[i] = CRGB(255, 90, 0);
+    FastLED.setBrightness(6*i);
+   FastLED.show();
+    delay(100);
+  }
+   for(int i=NUM_LEDS; i>0; i--){
+    leds[i] = CRGB(255, 40, 0);
+    FastLED.setBrightness(60-2*i)
+    FastLED.show();
+    delay(100);
+ } 
 
 
 }
